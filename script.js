1f13a146f2b944b51091fcc5a20f12202bcbf024
Mrcavasos <cavasos_anthony@hotmail.com>
2024-04-21 17:29:10 -0600

    assignment  set 3
    implementing smooth scrolling

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ document.addEventListener('keydown', function (e) {
 ////////////////////////////////////////////////
 /////////////////////////////////////////////
 ////////////////////////////////////////////////
-
+/*
 // This is how we SElECT ELEMENTS
 console.log(document.documentElement);
 console.log(document.head);
@@ -126,3 +126,35 @@ logo.classList.contains(''); /// not includes like arrays
 
 // Don't Use
 logo.clasName = 'tony';
+*/
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
