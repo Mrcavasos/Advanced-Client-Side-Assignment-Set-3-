2fd0aa712b03ece0153f172a403b8b6e15f774b7
Mrcavasos <cavasos_anthony@hotmail.com>
2024-04-30 12:59:13 -0600

    assignment set 3 section 14
    coding challenge

--- Section 14/script.js
+++ Section 14/script.js
@@ -8,8 +8,8 @@ const Person = function (firstName, birthYear) {
     console.log(2037 - this.birthYear);
   };
 };
-new Person('Jonas', 1991);
-console.log(jonas);
+const jonas = new Person('jonas', 1991);
+console.log('jonas');
 
 // 1. New {} is created
 // 2. function is called, this  = {}
@@ -17,19 +17,21 @@ console.log(jonas);
 // 4. function automatically return {}
 
 const matilda = new Person('Matilda', 2017);
-const jack = new Person('jack', 1975);
+const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
 
 console.log(jonas instanceof Person);
 
 // Prototypes
 
+console.log(Person.prototype);
+
 Person.prototype.calcAge = function () {
   console.log(2037 - this.birthYear);
 };
 
-jonas.calAge();
-matilda.calAge();
+jonas.calcAge();
+matilda.calcAge();
 
 console.log(jonas.__proto__);
 console.log(jonas.__proto__ === Person.prototype);
@@ -45,3 +47,64 @@ console.log(jonas.species, matilda.species);
 
 console.log(jonas.hasOwnProperty('firstName'));
 console.log(jonas.hasOwnProperty('species'));
+
+console.log(jonas.__proto__);
+
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 4, 5, 6, 9, 3]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.protoType);
+
+console.log(arr.__proto__.__proto__);
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+
+console.dir(x => x + 1);
+
+/////////////////////////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a  make and  a speed property. The speed property is the current speed of the car in km/h;
+2. Implement a 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Creat 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+*/
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.brake();
