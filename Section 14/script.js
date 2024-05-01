621a69b3236254bdc6a7e9bb3710626d704a8513
Mrcavasos <cavasos_anthony@hotmail.com>
2024-04-30 21:56:15 -0600

    assignment set 3 section 14
    ES6 Classes

--- Section 14/script.js
+++ Section 14/script.js
@@ -81,7 +81,7 @@ console.dir(x => x + 1);
 
 DATA CAR 1: 'BMW' going at 120 km/h
 DATA CAR 2: 'Mercedes' going at 95 km/h
-*/
+
 
 const Car = function (make, speed) {
   this.make = make;
@@ -108,3 +108,48 @@ bmw.brake();
 mercedes.accelerate();
 mercedes.accelerate();
 mercedes.brake();
+*/
+
+// class expression
+//const PersonCl = class {};
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+}
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ == PersonCl.prototype);
+
+PersonCl.prototype.greet = function () {
+  console.log(`Hey ${this.firstName}`);
+};
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Class are first-class citizens
+// 3. Classes are executed in strict mode
+
+const account = {
+  owner: 'jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log();
