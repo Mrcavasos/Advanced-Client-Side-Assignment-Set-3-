cfcc69527380d3954dbb05dea06ea5d2962e088e
Mrcavasos <cavasos_anthony@hotmail.com>
2024-05-01 15:54:09 -0600

    assignment set 3 section 14
    setters and getters

--- Section 14/script.js
+++ Section 14/script.js
@@ -124,10 +124,30 @@ class PersonCl {
   calcAge() {
     console.log(2037 - this.birthYear);
   }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+  get fullName() {
+    return this._fullName;
+  }
 }
 const jessica = new PersonCl('Jessica', 1996);
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
 
 console.log(jessica.__proto__ == PersonCl.prototype);
 
@@ -140,6 +160,8 @@ jessica.greet();
 // 2. Class are first-class citizens
 // 3. Classes are executed in strict mode
 
+const walter = new PersonCl('Walter White', 1965);
+
 const account = {
   owner: 'jonas',
   movements: [200, 530, 120, 300],
@@ -147,9 +169,13 @@ const account = {
   get latest() {
     return this.movements.slice(-1).pop();
   },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
 };
 
 console.log(account.latest);
 
 account.latest = 50;
-console.log();
+console.log(account.movements);
