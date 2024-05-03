b479989817b9e82e328afe23022b72ab7d51472b
Mrcavasos <cavasos_anthony@hotmail.com>
2024-05-02 18:42:01 -0600

    assignment set 3 section #14
    Inheritance Between "Classes": ES6 Classes

--- Section 14/script.js
+++ Section 14/script.js
@@ -305,7 +305,7 @@ console.dir(Student.prototype.constructor);
 4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definition of polymorphism
 
 DATA CAR 1: 'Tesla' going at 120km/h, with a charge of 23%
-*/
+
 
 const Car = function (make, speed) {
   this.make = make;
@@ -351,3 +351,69 @@ const cybertruck = new EV('Cybertruck', 120, 23);
 
 cybertruck.accelerate();
 cybertruck.brake();
+
+*/
+
+/////////////////////////////////////////////
+/// Inheritance Between "Classes": ES6 classes
+
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+  //Instance methods
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+  get fullName() {
+    return this._fullName;
+  }
+
+  // Static method
+  static hey() {
+    console.log('Hey there 👋');
+  }
+}
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    // Always needs to happen first!
+    super(fullName, birthYear);
+    this.course = course;
+  }
+  introduce() {
+    console.log(`My name is ${this.firstName} and I study ${this.course}`);
+  }
+
+  calcAge() {
+    console.log(
+      `I'm ${
+        2037 - this.birthYear
+      } years old, but as a student I feel more like ${
+        2037 - this.birthYear + 10
+      }`
+    );
+  }
+}
+
+const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
+martha.introduce();
+martha.calcAge();
