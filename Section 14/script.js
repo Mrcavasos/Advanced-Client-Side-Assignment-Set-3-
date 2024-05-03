2f5a97bc6be779f02b65a1e302c5b965fe705b01
Mrcavasos <cavasos_anthony@hotmail.com>
2024-05-02 19:05:57 -0600

    assignment set 3 section #14
    Inheritance Between classes Object.create

--- Section 14/script.js
+++ Section 14/script.js
@@ -352,7 +352,7 @@ const cybertruck = new EV('Cybertruck', 120, 23);
 cybertruck.accelerate();
 cybertruck.brake();
 
-*/
+
 
 /////////////////////////////////////////////
 /// Inheritance Between "Classes": ES6 classes
@@ -417,3 +417,31 @@ class StudentCl extends PersonCl {
 const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 martha.introduce();
 martha.calcAge();
+*/
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear, course);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.fulName} and I study ${this.course}`);
+};
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
